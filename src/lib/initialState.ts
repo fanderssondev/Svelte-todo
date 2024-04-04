71e3725211397a2fc398ae939d149a1a942415eb
Fredrik <fredrikanderssondev@gmail.com>
2024-04-04 16:20:57 +0200

    Updating

--- src/lib/initialState.ts
+++
@@ -1,6 +0,0 @@
-export const initialState: Todo[] = [
-   { id: '1e4a59703af84', text: 'Todo 1', completed: true },
-   { id: '9e09bcd7b9349', text: 'Todo 2', completed: false },
-   { id: '9e4273a51a37c', text: 'Todo 3', completed: false },
-   { id: '53ae48bf605cc', text: 'Todo 4', completed: false },
-];
